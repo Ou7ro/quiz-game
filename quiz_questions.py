5cbd60287d41fc9da58b3b76dd55b1cae9495262
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-11-28 15:01:17 +0500

    Убрал лишнюю глобальную переменную

--- quiz_questions.py
+++ quiz_questions.py
@@ -8,21 +8,20 @@ _questions = None
 
 
 def load_questions():
-    global _questions
     env.read_env()
     questions_path = env.str('QUESTION_PATH', 'questions.json')
     try:
         with open(questions_path, "r", encoding="KOI8-R") as file:
-            _questions = json.load(file)
+            questions = json.load(file)
         logger.info("Вопросы загружены")
-        return True
+        return questions
     except Exception as e:
         logger.error(f"Ошибка загрузки вопросов: {e}")
-        _questions = []
-        return False
+        return None
 
 
 def get_random_question():
-    if _questions is None:
+    questions = load_questions()
+    if not questions:
         raise Exception("Вопросы не загружены.")
-    return random.choice(_questions)
+    return random.choice(questions)
